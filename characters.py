846bbdb89a40696365f16050284875daa7ce9ee8
YEETEDWIN <edwinatyt@gmail.com>
2021-06-27 12:56:09 +0200

    commit on 'main'

--- characters.py
+++ characters.py
@@ -1,8 +1,9 @@
 #/bin/python3
-import random, time
+import random, time, sys
 from player import Player
 from getkey import getkey, keys
 from threading import Thread
+from operator import add, mul, sub, truediv
 
 # R.O.B. class
 class ROB(Player):
@@ -66,7 +67,6 @@ class ROB(Player):
       self.attack(enemy, 100)
       self.speed = 0
       self.moves.pop("diffusion beam")
-
 
 # Ganondorf class
 class Ganondorf(Player):
@@ -385,6 +385,7 @@ class Mario(Player):
     self.energy+=0.2
     self.attack(enemy, 30)
 
+
 # Falco class
 class Falco(Player):
   def __init__(self):
@@ -443,3 +444,141 @@ class Falco(Player):
       answer1 = input("Type in RIGHT\n")
 
 
+# Wolf class
+class Wolf(Player):
+  def __init__(self):
+    Player.__init__(self, "Wolf")
+    self.moves.pop("attack")
+    self.moves["blaster"] = self.attack
+    self.moves["wolf flash"] = self.flash
+    self.moves["reflector"] = self.reflector
+    self.moves["landmaster"] = self.landmaster
+
+  def flash(self, enemy):
+    self.energy += 0.3
+    print("I, "+ self.name +" shall speed up ")
+    print(self.name + " has increased energy.")
+    if self.energy >= 2:
+      self.moves.pop("wolf flash")
+  
+  def reflector(self, enemy):
+    if random.random() <= 0.2:
+      print("REFLECTIONNNNNN-!")
+      damage = enemy.health/2
+      round(damage)
+      self.attack(enemy, damage)
+    else:
+      damage = enemy.health/4
+      round(damage)
+      self.attack(enemy, damage)
+    
+  def landmaster(self, enemy):
+    answer1 = None
+    choices = ["ABOVE","DOWN","LEFT","RIGHT"]
+    direction1 = random.choice(choices)
+
+    def check1():
+      time.sleep(2)
+      if answer1 != None:
+        if answer1 == direction1:
+          self.attack(enemy, 50)
+          return
+        else:
+          print("Not correct choice you lose a turn \npress enter to continue")
+          return
+      print("Too slow, you lose a turn\npress enter to continue")
+
+    if direction1 == "ABOVE":
+      Thread(target = check1).start()
+      answer1 = input("Type in ABOVE\n>")
+    elif direction1 == "DOWN":
+      Thread(target = check1).start()
+      answer1 = input("Type in DOWN\n")
+    elif direction1 == "LEFT":
+      Thread(target = check1).start()
+      answer1 = input("Type in LEFT\n")
+    elif direction1 == "RIGHT":
+      Thread(target = check1).start()
+      answer1 = input("Type in RIGHT\n")
+
+
+# Meta Knight class
+class MetaKnight(Player):
+
+  def __init__(self):
+    Player.__init__(self, "Meta Knight")
+    self.moves.pop("attack")
+    self.moves["mach tornado"] = self.attack
+    self.moves["drill rush"] = self.drill_rush
+    self.moves.pop("heal")
+    self.moves["shuttle loop"] = self.shuttle_loop
+    self.moves["dimensional cape"] = self.cape
+    self.moves["galaxia darkness"] = self.darkness
+    self.movescount = 2
+
+  def drill_rush(self, enemy):
+    choices = ['UP','DOWN','RIGHT','LEFT']
+    print("Press up, down, right or left!")
+    direction = random.choice(choices)
+    key = getkey()
+    if key == keys.UP:
+      if direction == 'UP':
+        damage = random.randint(35,55)
+        print('Correct choice, extra damage')
+        time.sleep(1)
+      else:
+        damage = random.randint(15,25)
+        print('Incorrect choice, decrease damage')
+        time.sleep(1)
+    elif key == keys.DOWN:
+      if direction == 'DOWN':
+        damage = random.randint(35,55)
+        print('Correct choice, extra damage')
+        time.sleep(1)
+      else:
+        damage = random.randint(15,25)
+        print('Incorrect choice, decrease damage')
+        time.sleep(1)
+    elif key == keys.RIGHT:
+      if direction == 'RIGHT':
+        damage = random.randint(35,55)
+        print('Correct choice, extra damage')
+        time.sleep(1)
+      else:
+        damage = random.randint(15,25)
+        print('Incorrect choice, decrease damage')
+        time.sleep(1)
+    elif key == keys.LEFT:
+      if direction == 'LEFT':
+        damage = random.randint(35,55)
+        print('Correct choice, extra damage')
+        time.sleep(1)
+      else:
+        damage = random.randint(15,25)
+        print('Incorrect choice, decrease damage')
+        time.sleep(1)
+    else:
+      print('You wasted your turn by clicking something else')
+    self.attack(enemy, damage)
+
+  def shuttle_loop(self, enemy):
+    if random.random() <= 0.5:
+      self.attack(enemy, 20)
+    self.heal(self)
+
+  def cape(self, enemy):
+    self.energy += 0.2
+    print("I shall hide myself and increase my power")
+    print(self.name + " has increased energy.")
+    if self.energy >= 2:
+      self.moves.pop("dimensional cape")
+  
+  def darkness(self, enemy):
+    self.attack(enemy, 60)
+    self.movescount -= 1 # subtract 1 use of specialmove
+    if self.movescount > 0:
+      print(str(self.movescount) + " time left to use this move.")
+    else: # if 0 specialmove left
+      time.sleep(1)
+      print("You cannot use this move anymore.")
+      self.moves.pop("galaxia darkness") # remove from dict of possible moves
